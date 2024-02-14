8196d0debaa72bb4e6da7686029b4a7aadbb1f04
Gustavo Henklein <guvhm11@gmail.com>
2024-02-14 17:47:34 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,59 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var form = document.getElementById('form');
+  
+    form.addEventListener('submit', function (event) {
+      event.preventDefault(); // Evita o comportamento padrão de envio do formulário
+      calcularIngestao();
+    });
+  });
+  
+  function calcularIngestao() {
+    // Obter os valores do peso e idade
+    var peso = parseFloat(document.getElementById('weight').value);
+  
+    // Verificar se o valor do peso é um número válido
+    if (isNaN(peso)) {
+      alert('Por favor, insira um valor válido para o peso.');
+      return;
+    }
+  
+    // Calcular a ingestão hídrica (peso x 0.35)
+    var ingestaoHidrica = peso * 35;
+  
+    // Exibir os resultados na tela
+    var infosElement = document.getElementById('infos');
+    infosElement.classList.remove('hidden');
+    
+    var valueElement = document.getElementById('value');
+    valueElement.innerText = ingestaoHidrica.toFixed(0).replace('.',',') + 'ml';
+
+  
+    
+    
+  }
+
+  // function calcularImc() {
+  //   // Obter os valores do peso e altura
+  //   var peso = parseFloat(document.getElementById('weight').value);
+  //   var altura = parseFloat(document.getElementById('height').value);
+  
+  //   // Verificar se o valor do peso/altura é um número válido
+  //   if (isNaN(peso && altura)) {
+  //     alert('Por favor, insira um valor válido para o peso.');
+  //     return;
+  //   }
+  
+  //   // Calcular o IMC
+  //   var calculoImc = (peso /(altura * altura));
+  
+  //   // Exibir os resultados na tela
+  //   var infosElement = document.getElementById('infos');
+  //   infosElement.classList.remove('hidden');
+    
+  //   var valueElement = document.getElementById('value');
+  //   valueElement.innerText = calculoImc.toFixed(0).replace('.',',') + 'ml';
+
+  
+   
+  // }
+  
